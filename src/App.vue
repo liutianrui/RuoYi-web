12871c0a765c2e360d526eddccd1a8512d432bce
liutianrui <373475284@qq.com>
2023-06-28 23:36:46 +0800

    logo更改

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@
     <!--  用来兼容乾坤前端微服务  -->
     <div id="qiankun"></div>
     <!-- 授权后可以删除-->
-    <div class="dvadmin-auth">
-      <span>Powered by Django-Vue-Admin</span>
-      <el-divider direction="vertical"></el-divider>
-      <span>Copyright 好同学开发团队</span>
-    </div>
+<!--    <div class="dvadmin-auth">-->
+<!--      <span>Powered by Django-Vue-Admin</span>-->
+<!--      <el-divider direction="vertical"></el-divider>-->
+<!--      <span>Copyright 好同学开发团队</span>-->
+<!--    </div>-->
   </div>
 </template>
 
